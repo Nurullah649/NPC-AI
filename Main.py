0868efba645a1c0dc27229fb464fbe073568020c
Nurullah649 <f221220013@ktun.edu.tr>
2024-02-22 15:25:21 +0300

    train deneme 3

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
+from PIL import Image
 
 # Load a model
 model = YOLO('yolov8n.pt')  #Load pretrained model
 train_yaml="/home/nurullah/Masaüstü/NPC-AI/config.yaml"
-# Train the model
-results = model.train(data=train_yaml, epochs=5, imgsz=640)
+im1=Image.open('8d4a2e97-1697-4ffa-8c1e-05a5cc0143c5.jpeg')
+result = model.train(data=train_yaml, epochs=5, imgsz=640,save=True)# Train the model
+#result=model.predict(source=im1,save=True)
