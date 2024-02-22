017aaa5fbc671c966c650df87906624e354295e4
Nurullah649 <f221220013@ktun.edu.tr>
2024-02-22 14:45:38 +0300

    train deneme 2

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8n.pt')  # build a new model from YAML
+model = YOLO('yolov8n.pt')  #Load pretrained model
 train_yaml="/home/nurullah/Masaüstü/NPC-AI/config.yaml"
 # Train the model
-results = model.train(data=train_yaml, epochs=1, imgsz=640)
+results = model.train(data=train_yaml, epochs=5, imgsz=640)
