63dd6dc6783507a9e11818f1d03bbd3db79c1bcd
Nurullah649 <f221220013@ktun.edu.tr>
2024-02-28 12:23:53 +0300

    .

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 from PIL import Image
 
 # Load a model
-model = YOLO('yolov8n.pt')  #Load pretrained model
+model = YOLO('/home/nurullah/Masaüstü/NPC-AI/yolov8n.pt')  #Load pretrained model
 train_yaml="/home/nurullah/Masaüstü/NPC-AI/config.yaml"
-im1=Image.open('8d4a2e97-1697-4ffa-8c1e-05a5cc0143c5.jpeg')
-result = model.train(data=train_yaml, epochs=5, imgsz=640,save=True)# Train the model
+#im1=Image.open('8d4a2e97-1697-4ffa-8c1e-05a5cc0143c5.jpeg')
+result = model.train(data="/home/nurullah/Masaüstü/NPC-AI/config.yaml", epochs=5, imgsz=640,save=True)# Train the model
 #result=model.predict(source=im1,save=True)
