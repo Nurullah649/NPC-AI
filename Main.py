faa80f971955f2657e35bf18e4f193073ab392a7
Nurullah649 <f221220013@ktun.edu.tr>
2024-03-03 19:04:17 +0300

    .

--- Main.py
+++ Main.py
@@ -1,17 +1,8 @@
-import gc
-import glob
-from transformers import AutoTokenizer
 from ultralytics import YOLO
-import  yaml
-with open("config.yaml", "r") as dosya:
-    veri = yaml.safe_load(dosya)
-names = veri.get("names")
-list1=[names]
-# Load a model
+
 model = YOLO('/home/nurullah/Masaüstü/NPC-AI/yolov8n.pt')  #Load pretrained model
-a=0
 train_yaml="/home/nurullah/Masaüstü/NPC-AI/config.yaml"
-#model.train(data=train_yaml, epochs=1, imgsz=640,save=True,save_txt=True)# Train the model
+model.train(data=train_yaml, epochs=1, imgsz=640,save=True,save_txt=True)# Train the model
 
 
 
--- detect_with_custom_pt.py
+++ detect_with_custom_pt.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 a=0
 model = YOLO('/home/nurullah/Masaüstü/NPC-AI/runs/detect/train33/weights/best.pt')
 for fname in glob.glob("/home/nurullah/Masaüstü/NPC-AI/DENEME_DATA/images/train/*.jpg"):
-    model(fname,data='/home/nurullah/Masaüstü/NPC-AI/runs/detect/train33/weights/best.pt',save_txt=True,save=True)
+    model(fname,data='/home/nurullah/Masaüstü/NPC-AI/runs/detect/train38/weights/best.pt',save_txt=True,save=True)
     a += 1
     if (a == 100):
         gc.collect()
