ebe0d95e46a2deb6e05995ed81b9de84be15fcf0
Nurullah649 <f221220013@ktun.edu.tr>
2024-03-04 18:59:46 +0300

    .

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('/home/nurullah/Masaüstü/NPC-AI/yolov8n.pt')  #Load pretrained model
+model = YOLO('/home/nurullah/Masaüstü/NPC-AI/runs/detect/train41/weights/best.pt')  #Load pretrained model
 train_yaml="/home/nurullah/Masaüstü/NPC-AI/config.yaml"
 model.train(data=train_yaml, epochs=1, imgsz=640,save=True,save_txt=True)# Train the model
 
