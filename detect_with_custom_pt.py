d18e930419311c1200ed6bea6cf5a0a280ff2476
Nurullah649 <f221220013@ktun.edu.tr>
2024-03-27 12:38:24 +0300

    ..

--- detect_with_custom_pt.py
+++ detect_with_custom_pt.py
@@ -1,19 +1,17 @@
 import gc
 import glob
 
-import cv2
 from ultralytics import YOLO
-from PIL import Image
 gc.collect()
 a=0
 model = YOLO('/home/nurullah/Masaüstü/NPC-AI/runs/detect/train4/weights/best.pt')
-result=model.predict('/home/nurullah/İndirilenler/2022_dataset-20240308T144120Z-001/2022_dataset/train/images/2022_pexels-tom-fisk-9832125.mp4',data='/home/nurullah/Masaüstü/NPC-AI/runs/detect/train5/weights/best.pt',save_txt=True,save=True)
+#result=model.predict('/home/nurullah/İndirilenler/2022_dataset-20240308T144120Z-001/2022_dataset/train/images/2022_pexels-tom-fisk-9832125.mp4',data='/home/nurullah/Masaüstü/NPC-AI/runs/detect/train5/weights/best.pt',save_txt=True,save=True)
 
-"""for fname in glob.glob("/home/nurullah/Masaüstü/datasets/VisDrone/VisDrone2019-VID-train/sequences/uav0000013_00000_v/*.jpg"):
+for fname in glob.glob("/home/nurullah/Masaüstü/NPC-AI/bedir/M1201/*.jpg"):
 
-    result=model.predict(fname,confidence_threshold=0.35,data='/home/nurullah/Masaüstü/NPC-AI/runs/detect/train5/weights/best.pt',save_txt=True,save=True)
+    result=model.predict(fname,data='/home/nurullah/Masaüstü/NPC-AI/runs/detect/train5/weights/best.pt',save_txt=True,save=True)
 
     a+=1
     if(a==100):
-        
-        a=0"""
+        gc.collect()
+        a=0
