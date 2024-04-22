4edb984f0dac1abd9c9edca0ab1088ffcddc8411
Nurullah649 <f221220013@ktun.edu.tr>
2024-04-22 13:40:53 +0300

    ..

--- predict.py
+++ predict.py
@@ -1,20 +1,35 @@
-from ultralytics import YOLO
-import gc
-from os.path import expanduser
+import cv2
+import glob
 import os
-import torch
+import time
+from ultralytics import YOLO
+from os.path import expanduser
+from Nurullah_scripts import CLAHE
+from colorama import Fore, Style
 
 # Kullanıcı masaüstü dizinini al
 desktop_path = os.path.join(expanduser("~"), "Masaüstü")
-model = YOLO(desktop_path+'/NPC-AI/runs/detect/train8/weights/best.pt')#Load pretrained model
-
-train_yaml=desktop_path+"/NPC-AI/config.yaml"
-model.predict(source="/home/npc-ai/Masaüstü/TUYZ_2024_Ornek_Veri-001/TUYZ_2024_Ornek_Veri/frames/*.jpg",conf=0.40,data=train_yaml,save=True,save_txt=True)# Train the model
-
-
-
-
-
-
-
+model = YOLO(desktop_path + '/NPC-AI/runs/detect/train8/weights/best.pt')  # Pretrained model path
+path = "/home/nurullah/İndirilenler/UAV-benchmark-M/M0402/*jpg"
+train_yaml = desktop_path + "/NPC-AI/config.yaml"
+start_for_time = time.time()
+counter=1
+for fname in glob.glob(path):
+    start_time = time.time()
+    if counter==1:
+        a = (start_time - start_for_time) * 1000
+        print(Fore.RED + f"{a}" + Style.RESET_ALL)
+    counter+=1
+    img=CLAHE.clahe(fname)
+    results = model(
+        source=img,
+        conf=0.40,
+        data=train_yaml,
+        save=True,
+        save_txt=True
+    )
+    print(os.path.basename(img)," ",Fore.MAGENTA,results,Style.RESET_ALL)
+    end_time = time.time()
+    elapsed_time = (end_time - start_time) * 1000
+    print(Fore.CYAN + f"Function took {elapsed_time} milliseconds to execute." + Style.RESET_ALL)
 
