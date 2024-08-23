5a2bea1cd0bbbdda65653ed66a17cbef1397e936
Nurullah649 <113285824+Nurullah649@users.noreply.github.com>
2024-08-23 14:08:41 +0300

    Update ORB_Güncelleme_Deneme_kodu.py

--- ORB_Güncelleme_Deneme_kodu.py
+++ ORB_Güncelleme_Deneme_kodu.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 # ORB ve gerekli değişkenlerin tanımlanması
-orb = cv2.ORB.create()
+orb = cv2.ORB.create(nfeatures=1000,WTA_K=3,scaleFactor=1.05,edgeThreshold=15)
 # Dosya yolunu belirtin
 file_path = 'data/2024_TUYZ_Online_Yarisma_Ana_Oturum.csv'
 
@@ -27,13 +27,11 @@ prev_kp = None
 scale_factor = None  # Ölçeklendirme faktörü
 offset = None  # Offset değeri
 
-
-
-
 # Frame dizini ve sıralama
 frames_path = '../Predict/2024_TUYZ_Online_Yarisma_Oturumu/2024_TUYZ_Online_Yarisma_Ana_Oturum/'
 frames = sorted(os.listdir(frames_path), key=lambda x: int(x.split('_')[1].split('.')[0]))
-alg_positions=[]
+alg_positions = []
+
 def process_frame(frame, count, frame_name):
     global is_first_frame, current_position, positions, current_angle, prev_des, prev_kp, scale_factor, offset, alg_positions
     camera_matrix = np.array([
@@ -58,11 +56,18 @@ def process_frame(frame, count, frame_name):
                 src_mean = np.mean(src_pts, axis=0)
                 dst_mean = np.mean(dst_pts, axis=0)
                 movement = dst_mean - src_mean
-                current_position += movement.flatten()
 
                 # Açısal değişimi hesapla
                 angle_rad = np.arctan2(M[1, 0], M[0, 0])
                 current_angle += np.degrees(angle_rad)
+
+                # Hareket vektörünü açısal değişime göre döndür
+                rotation_matrix = np.array([
+                    [np.cos(angle_rad), -np.sin(angle_rad)],
+                    [np.sin(angle_rad), np.cos(angle_rad)]
+                ])
+                movement_rotated = np.dot(rotation_matrix, movement.flatten())
+                current_position += movement_rotated
 
                 # Pozisyonları güncelle ve X ve Y eksenlerini ters çevir
                 positions = current_position.copy() * np.array([-1, -1])
@@ -72,7 +77,7 @@ def process_frame(frame, count, frame_name):
             positions = current_position.copy() * np.array([-1, -1])
     else:
         print("First frame")
-        positions=np.array([xy_data[0][0], xy_data[0][1]])
+        positions = np.array([xy_data[0][0], xy_data[0][1]])
 
     prev_des = des2
     prev_kp = kp2
@@ -81,7 +86,7 @@ def process_frame(frame, count, frame_name):
 
     if count >= 449:
         if scale_factor is None:
-            model = LinearRegression(fit_intercept=False,positive=False)
+            model = LinearRegression(fit_intercept=False, positive=False)
             model.fit(alg_positions, xy_data)
             scale_factor = model.coef_
             offset = model.intercept_
@@ -93,7 +98,7 @@ def process_frame(frame, count, frame_name):
         pred_translation_x = xy_data[count][0]
         pred_translation_y = xy_data[count][1]
 
-    with open("data/Result_2.txt", 'a') as file:
+    with open("data/Result_3.txt", 'a') as file:
         file.write(f"{pred_translation_x}, {pred_translation_y}, {frame_name}\n")
 
 count = 0
@@ -104,4 +109,3 @@ for frame in frames:
     frame = cv2.imread(frame_path)
     process_frame(frame, count, frame_name)
     count += 1
-
