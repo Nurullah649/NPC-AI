dfeaaff46ca8ae13ef15772c28e83c69ea05e853
Nurullah649 <113285824+Nurullah649@users.noreply.github.com>
2024-08-26 15:35:47 +0300

    Positioning Update

--- Positioning/ORB_UPDATE.py
+++ Positioning/ORB_UPDATE.py
@@ -2,38 +2,115 @@ import os
 import cv2
 import numpy as np
 import pandas as pd
+from scipy.optimize import leastsq, least_squares
 from sklearn.linear_model import LinearRegression
+from filterpy.kalman import KalmanFilter
+#from Class.R_Model import R_Model
 
-# ORB ve gerekli değişkenlerin tanımlanması
-orb = cv2.ORB.create(nfeatures=1050,WTA_K=3,scaleFactor=1.035,edgeThreshold=14,nlevels=9)
-# Dosya yolunu belirtin
-file_path = '../data/2024_TUYZ_Online_Yarisma_Ana_Oturum.csv'
 
-# CSV dosyasını yükle
-df = pd.read_csv(file_path)
-# İlk 450 x ve y verisini seçin (x ve y sütunlarını belirleyin)
-x_sutunu_indeksi = 0
-y_sutunu_indeksi = 1
-xy_data = df.iloc[:450, [x_sutunu_indeksi, y_sutunu_indeksi]].values
+def train_model(alg_postions, gt_positions, scale_factor, offset):
+    with open("../Sonuc/ALG_data_model_train.csv", 'a') as file:
+        file.write("x,y\n")
+        for position in alg_postions:
+            scaled_positions = np.dot(position, scale_factor.T) + offset
+            pred_translation_x = scaled_positions[0]
+            pred_translation_y = scaled_positions[1]
+            file.write(f"{pred_translation_x},{pred_translation_y}\n")
 
-positions = np.array([0.0, 0.0])
-current_position = np.array([xy_data[0][0], xy_data[0][1]])
-current_angle = 0.0
-is_first_frame = True
-prev_des = None
-prev_kp = None
+    with open("../Sonuc/GT_data_model_train.csv", 'a') as file:
+        file.write("x,y\n")
+        for position in gt_positions:
+            file.write(f"{position[0]},{position[1]}\n")
+    # r_model = R_Model(alg_data_path="R_model_train.csv",gt_data_path="GT_model_train.csv")
+    # return r_model
 
-# Ölçeklendirme ve offset değerlerini tanımlayın
-scale_factor = None  # Ölçeklendirme faktörü
-offset = None  # Offset değeri
 
-# Frame dizini ve sıralama
-frames_path = '../../Predict/2024_TUYZ_Online_Yarisma_Oturumu/2024_TUYZ_Online_Yarisma_Ana_Oturum/'
-frames = sorted(os.listdir(frames_path), key=lambda x: int(x.split('_')[1].split('.')[0]))
-alg_positions = []
+def initialize_orb():
+    return cv2.ORB.create(nfeatures=1050, WTA_K=3, scaleFactor=1.035, edgeThreshold=14, nlevels=9)
 
-def process_frame(frame, count, frame_name):
-    global is_first_frame, current_position, positions, current_angle, prev_des, prev_kp, scale_factor, offset, alg_positions
+
+def initialize_kalman_filter(initial_position):
+    kf = KalmanFilter(dim_x=4, dim_z=2)
+    kf.x = np.array([initial_position[0], 0., initial_position[1], 0.])
+    kf.F = np.array([
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1]
+    ])
+    kf.H = np.array([
+        [1, 0, 0, 0],
+        [0, 0, 1, 0]
+    ])
+    kf.P *= 50000
+    kf.R = np.array([
+        [50000, 0],
+        [0, 50000]
+    ])
+    kf.Q = np.eye(4)
+    return kf
+
+
+def kalman_update(kf, measured_position):
+    kf.predict()
+    kf.update(measured_position)
+    return kf.x[0], kf.x[2]
+
+
+def match_features(prev_des, des2, prev_kp, kp2):
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(prev_des, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    src_pts = np.float32([prev_kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+    return src_pts, dst_pts
+
+
+def residuals(h, src_pts, dst_pts):
+    h_matrix = h.reshape((3, 3))
+    projected_pts = cv2.perspectiveTransform(src_pts, h_matrix)
+    return (dst_pts - projected_pts).flatten()
+
+
+def optimize_homography(m, src_pts, dst_pts):
+    initial_h = m.flatten()
+    optimized_h, _ = leastsq(residuals, initial_h, args=(src_pts, dst_pts))
+    optimized_h_matrix = optimized_h.reshape((3, 3))
+    return optimized_h_matrix
+
+
+def update_position(src_pts, dst_pts, current_position, current_angle):
+    positions = current_position.copy()
+    if len(src_pts) >= 4 and len(dst_pts) >= 4:
+
+        m, mask = cv2.findHomography(src_pts, dst_pts,cv2.RANSAC,ransacReprojThreshold=3.0)
+        if m is not None:
+            m = optimize_homography(m, src_pts, dst_pts)  # LM ile optimizasyon
+            src_mean = np.mean(src_pts, axis=0)
+            dst_mean = np.mean(dst_pts, axis=0)
+            movement = dst_mean - src_mean
+            angle_rad = np.arctan2(m[1, 0], m[0, 0])
+            if np.abs(np.degrees(angle_rad)) > 1:
+                current_angle += np.degrees(angle_rad)
+            rotation_matrix = np.array([
+                [np.cos(angle_rad), -np.sin(angle_rad)],
+                [np.sin(angle_rad), np.cos(angle_rad)]
+            ])
+            movement_rotated = np.dot(rotation_matrix, movement.flatten())
+            positions += movement_rotated
+    return positions, current_angle
+
+
+def calculate_scaling_factors(alg_positions, xy_data):
+    model = LinearRegression(fit_intercept=False, positive=False)
+    model.fit(alg_positions, xy_data)
+    scale_factor = model.coef_
+    offset = model.intercept_
+    return scale_factor, offset
+
+
+def process_frame(frame, count, frame_name, xy_data, orb, kf, prev_des, prev_kp, current_position, current_angle,
+                  alg_positions, scale_factor, offset):
     camera_matrix = np.array([
         [1.4133e+03, 0, 950.0639],
         [0, 1.4188e+03, 543.3796],
@@ -45,67 +122,71 @@ def process_frame(frame, count, frame_name):
     kp2, des2 = orb.detectAndCompute(undistorted_frame, None)
 
     if prev_des is not None:
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(prev_des, des2)
-        matches = sorted(matches, key=lambda x: x.distance)
-        src_pts = np.float32([prev_kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-        if len(src_pts) >= 4 and len(dst_pts) >= 4:
-            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            if M is not None:
-                src_mean = np.mean(src_pts, axis=0)
-                dst_mean = np.mean(dst_pts, axis=0)
-                movement = dst_mean - src_mean
+        src_pts, dst_pts = match_features(prev_des, des2, prev_kp, kp2)
+        current_position, current_angle = update_position(src_pts, dst_pts, current_position, current_angle)
 
-                # Açısal değişimi hesapla
-                angle_rad = np.arctan2(M[1, 0], M[0, 0])
-                current_angle += np.degrees(angle_rad)
-
-                # Hareket vektörünü açısal değişime göre döndür
-                rotation_matrix = np.array([
-                    [np.cos(angle_rad), -np.sin(angle_rad)],
-                    [np.sin(angle_rad), np.cos(angle_rad)]
-                ])
-                movement_rotated = np.dot(rotation_matrix, movement.flatten())
-                current_position += movement_rotated
-
-                # Pozisyonları güncelle ve X ve Y eksenlerini ters çevir
-                positions = current_position.copy() * np.array([-1, -1])
-            else:
-                positions = current_position.copy() * np.array([-1, -1])
-        else:
-            positions = current_position.copy() * np.array([-1, -1])
     else:
-        print("First frame")
-        positions = np.array([xy_data[0][0], xy_data[0][1]])
+        current_position = np.array([xy_data[0][0], xy_data[0][1]])
 
     prev_des = des2
     prev_kp = kp2
-    is_first_frame = False
-    alg_positions.append(positions)
+    alg_positions.append(current_position)
 
-    if count >= 449:
-        if scale_factor is None:
-            model = LinearRegression(fit_intercept=False, positive=False)
-            model.fit(alg_positions, xy_data)
-            scale_factor = model.coef_
-            offset = model.intercept_
-        print(positions)
-        scaled_positions = np.dot(positions, scale_factor.T) + offset
+    kalman_position = kalman_update(kf, current_position)
+
+    if count >= 449 and scale_factor is None:
+        scale_factor, offset = calculate_scaling_factors(alg_positions, xy_data)
+        train_model(alg_positions, xy_data, scale_factor, offset)
+
+    if scale_factor is not None:
+        scaled_positions = np.dot(kalman_position, scale_factor.T) + offset
         pred_translation_x = scaled_positions[0]
         pred_translation_y = scaled_positions[1]
+        # pred_translation_x,pred_translation_y=r_model.predict(pred_translation_x,pred_translation_y)
     else:
         pred_translation_x = xy_data[count][0]
         pred_translation_y = xy_data[count][1]
 
-    with open("data/Result.txt", 'a') as file:
+    with open("../Sonuc/Result.txt", 'a') as file:
         file.write(f"{pred_translation_x}, {pred_translation_y}, {frame_name}\n")
 
-count = 0
-for frame in frames:
-    frame_path = os.path.join(frames_path, frame)
-    frame_name = os.path.basename(frame_path)
-    print(f"Processing frame: {frame_name}")
-    frame = cv2.imread(frame_path)
-    process_frame(frame, count, frame_name)
-    count += 1
+    return prev_des, prev_kp, current_position, current_angle, scale_factor, offset
+
+
+def main():
+    orb = initialize_orb()
+    file_path = '../data/2024_TUYZ_Online_Yarisma_Ana_Oturum.csv'
+    df = pd.read_csv(file_path)
+    x_sutunu_indeksi = 0
+    y_sutunu_indeksi = 1
+    xy_data = df.iloc[:450, [x_sutunu_indeksi, y_sutunu_indeksi]].values
+
+    positions = np.array([0.0, 0.0])
+    current_position = np.array([xy_data[0][0], xy_data[0][1]])
+    current_angle = 0.0
+    is_first_frame = True
+    prev_des = None
+    prev_kp = None
+    scale_factor = None
+    offset = None
+
+    frames_path = '../../Predict/2024_TUYZ_Online_Yarisma_Oturumu/2024_TUYZ_Online_Yarisma_Ana_Oturum/'
+    frames = sorted(os.listdir(frames_path), key=lambda x: int(x.split('_')[1].split('.')[0]))
+    alg_positions = []
+
+    kf = initialize_kalman_filter(current_position)
+
+    count = 0
+    for frame in frames:
+        frame_path = os.path.join(frames_path, frame)
+        frame_name = os.path.basename(frame_path)
+        print(f"Processing frame: {frame_name}")
+        frame = cv2.imread(frame_path)
+        prev_des, prev_kp, current_position, current_angle, scale_factor, offset = process_frame(
+            frame, count, frame_name, xy_data, orb, kf, prev_des, prev_kp, current_position,
+            current_angle, alg_positions, scale_factor, offset)
+        count += 1
+
+
+if __name__ == "__main__":
+    main()
