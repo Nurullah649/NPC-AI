ae5c0d860c446b40b5acf1adbe2437ecc7b8f9f9
Nurullah649 <113285824+Nurullah649@users.noreply.github.com>
2024-11-26 16:54:12 +0300

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageEnhance
+import random
 
 
 # Görüntüye Gaussian Noise ekleme
@@ -32,7 +33,11 @@ def add_blur(image, ksize=15):
 # Renk değişimi (Hue) ekleme
 def change_color(image, hue_shift=50):
     hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+
+    # Hue kanalındaki değerlerin 0 ile 179 arasında kalması için mod işlemi
     hsv_image[:, :, 0] = (hsv_image[:, :, 0] + hue_shift) % 180  # Hue'yu değiştir
+
+    # HSV -> RGB dönüşümünü yap
     color_changed_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2RGB)
     return color_changed_image
 
@@ -48,8 +53,15 @@ def change_brightness_contrast(image, brightness=1.5, contrast=1.5):
 
 
 # Tüm işlemleri birleştiren fonksiyon
-def augment_image(image, noise_type="gaussian", noise_std=25, blur_ksize=15, hue_shift=50, brightness=1.5,
-                  contrast=1.5):
+def augment_image(image):
+    # Parametreleri rasgele seç
+    noise_type = random.choice(["gaussian", "salt_and_pepper"])  # Gürültü tipi (Salt-and-pepper ya da Gaussian)
+    noise_std = random.randint(10, 100)  # Gaussian gürültüsü için standart sapma
+    blur_ksize = random.choice([5, 7, 9, 11, 13, 15])  # Bulanıklık için kernel boyutu
+    hue_shift = random.randint(0, 128)  # Hue değişimi için random bir kayma
+    brightness = round(random.uniform(1.0, 2.0), 2)  # Parlaklık için 1.0 ile 2.0 arasında rastgele bir değer
+    contrast = round(random.uniform(1.0, 2.0), 2)  # Kontrast için 1.0 ile 2.0 arasında rastgele bir değer
+
     # Parazit ekleme
     if noise_type == "gaussian":
         image = add_noise(image, std=noise_std)
@@ -69,12 +81,12 @@ def augment_image(image, noise_type="gaussian", noise_std=25, blur_ksize=15, hue
 
 
 # Görüntüyü yükle
-image = cv2.imread('../../Predict/images/2021/TUYZ_2021_Ornek_Veri_Seti/frame_000000.jpg')
+image = cv2.imread(
+    '../Downloads/2024_TUYZ_Online_Yarisma_Oturumu/2024_TUYZ_Online_Yarisma_Ana_Oturum/frame_000000.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 # Veri artırımı işlemi uygulama
-augmented_image = augment_image(image, noise_type="gaussian", noise_std=50, blur_ksize=15, hue_shift=60, brightness=1.7,
-                                contrast=1.5)
+augmented_image = augment_image(image)
 
 # Görüntüleri göster
 plt.figure(figsize=(10, 5))
@@ -89,5 +101,3 @@ plt.imshow(augmented_image)
 plt.axis('off')
 
 plt.show()
-
-
